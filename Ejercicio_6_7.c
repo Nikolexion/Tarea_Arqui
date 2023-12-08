5604d0ec5c2bd9bee16e008fc7aeb9a843f897ad
Nikolexion <nfalc2003@gmail.com>
2023-12-08 19:54:57 -0400

    Arreglados algunos bugs de la funcion invertir y sus versiones optimizadas

--- Ejercicio_6_7.c
+++ Ejercicio_6_7.c
@@ -4,10 +4,9 @@
 
 void invertir(char *s) {
     int i;
-    char temp1;
 
-    for (i = 0; i < strlen(s) - 1; i++) {
-        temp1 = s[i];
+    for (i = 0; i < strlen(s)/2; i++) {
+        char temp1 = s[i];
         s[i] = s[strlen(s) - 1 - i];
         s[strlen(s) - 1 - i] = temp1;
 
@@ -20,12 +19,12 @@ void invertir_optimizado_1(char *s) {
     char temp1;
     char temp2;
 
-    for (i = 0; i < length - 2; i+=2) {
+    for (i = 0; i < length/2; i+=2) {
         temp1 = s[i];
         temp2 = s[i+1];
   
         s[i] = s[length - 1 - i];
-        s[i+1] = s[length - 1 - i];
+        s[i+1] = s[length - 2 - i];
 
         s[length - 1 - i] = temp1;
         s[length - 2 - i] = temp2;
@@ -40,7 +39,7 @@ void invertir_optimizado_2(char *s) {
     char temp2;
     char temp3;
 
-    for (i = 0; i < length - 3; i+=3) {
+    for (i = 0; i < length/2; i+=3) {
         temp1 = s[i];
         temp2 = s[i+1];
         temp3 = s[i+2];
@@ -65,7 +64,7 @@ void invertir_optimizado_3(char *s) {
     char temp3;
     char temp4;
 
-    for (i = 0; i < length - 4; i+=4) {
+    for (i = 0; i < length/2; i+=4) {
         temp1 = s[i];
         temp2 = s[i+1];
         temp3 = s[i+2];
@@ -87,9 +86,8 @@ void invertir_optimizado_3(char *s) {
 
 
 int main() {
-    char str[20];
-    fgets(str,20,stdin);
-    //printf("Original: %s\n", str);
+    char str[500];
+    fgets(str,500,stdin);
     clock_t ini1,fin1,ini2,fin2,ini3,fin3,ini4,fin4;
 
     ini1 = clock();
@@ -100,8 +98,8 @@ int main() {
     }
     
     fin1 = clock();
+    //printf("\nInvertido: %s\n", str);
     printf("\nMilisegundos sin optimizar: %d", fin1-ini1);
-    //printf("Invertido: %s\n", str);
 
     ini2 = clock();
 
@@ -111,8 +109,8 @@ int main() {
     }
     
     fin2 = clock();
+    //printf("\nInvertido: %s\n", str);
     printf("\nMilisegundos optimizado1: %d",  fin2-ini2);
-    //printf("Invertido: %s\n", str);
     ini3 = clock();
 
     for (int i = 0; i < 1000001; i++)
@@ -121,8 +119,8 @@ int main() {
     }
     
     fin3 = clock();
+    //printf("\nInvertido: %s\n", str);
     printf("\nMilisegundos optimizado2: %d",  fin3-ini3);
-    //printf("Invertido: %s\n", str);
 
     ini4 = clock();
 
@@ -132,8 +130,9 @@ int main() {
     }
     
     fin4 = clock();
-    //printf("Invertido: %s\n", str);
-    printf("\nMilisegundos optimizado3: %d",  fin4-ini4);
 
+    //printf("\nInvertido: %s\n", str);
+    printf("\nMilisegundos optimizado3: %d",  fin4-ini4);
+    
     return 0;
 }
