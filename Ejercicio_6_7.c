b7e71ba3c7c4e497a61165f008c9d3414c620a49
Nikolexion <nfalc2003@gmail.com>
2023-12-08 15:53:15 -0400

    Ahora no solo se ocupa clock()

---
+++ Ejercicio_6_7.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+void invertir(char *s) {
+    int i;
+    char temp1;
+
+    for (i = 0; i < strlen(s) - 1; i++) {
+        temp1 = s[i];
+        s[i] = s[strlen(s) - 1 - i];
+        s[strlen(s) - 1 - i] = temp1;
+
+    }
+}
+
+void invertir_optimizado_1(char *s) {
+    int length = strlen(s);
+    int i;
+    char temp1;
+    char temp2;
+
+    for (i = 0; i < length - 2; i+=2) {
+        temp1 = s[i];
+        temp2 = s[i+1];
+  
+        s[i] = s[length - 1 - i];
+        s[i+1] = s[length - 1 - i];
+
+        s[length - 1 - i] = temp1;
+        s[length - 2 - i] = temp2;
+
+    }
+}
+
+void invertir_optimizado_2(char *s) {
+    int length = strlen(s);
+    int i;
+    char temp1;
+    char temp2;
+    char temp3;
+
+    for (i = 0; i < length - 3; i+=3) {
+        temp1 = s[i];
+        temp2 = s[i+1];
+        temp3 = s[i+2];
+  
+        s[i] = s[length - 1 - i];
+        s[i+1] = s[length - 2 - i];
+        s[i+2] = s[length - 3 - i];
+
+        s[length - 1 - i] = temp1;
+        s[length - 2 - i] = temp2;
+        s[length - 3 - i] = temp3;
+
+    }
+}
+
+
+void invertir_optimizado_3(char *s) {
+    int length = strlen(s);
+    int i;
+    char temp1;
+    char temp2;
+    char temp3;
+    char temp4;
+
+    for (i = 0; i < length - 4; i+=4) {
+        temp1 = s[i];
+        temp2 = s[i+1];
+        temp3 = s[i+2];
+        temp4 = s[i+3];
+  
+        s[i] = s[length - 1 - i];
+        s[i+1] = s[length - 2 - i];
+        s[i+2] = s[length - 3 - i];
+        s[i+3] = s[length - 4 - i];
+
+        s[length - 1 - i] = temp1;
+        s[length - 2 - i] = temp2;
+        s[length - 3 - i] = temp3;
+        s[length - 4 - i] = temp4;
+
+    }
+}
+
+
+
+int main() {
+    char str[20];
+    fgets(str,20,stdin);
+    //printf("Original: %s\n", str);
+    clock_t ini1,fin1,ini2,fin2,ini3,fin3,ini4,fin4;
+    srand(time(NULL));
+
+    ini1 = clock();
+
+    for (int i = 0; i < 1000001; i++)
+    {
+        invertir(str);
+    }
+    
+    fin1 = clock();
+    printf("\nMilisegundos sin optimizar: %d", fin1-ini1);
+    //printf("Invertido: %s\n", str);
+
+    ini2 = clock();
+
+    for (int i = 0; i < 1000001; i++)
+    {
+        invertir_optimizado_1(str);
+    }
+    
+    fin2 = clock();
+    printf("\nMilisegundos optimizado1: %d",  fin2-ini2);
+    //printf("Invertido: %s\n", str);
+    ini3 = clock();
+
+    for (int i = 0; i < 1000001; i++)
+    {
+        invertir_optimizado_2(str);
+    }
+    
+    fin3 = clock();
+    printf("\nMilisegundos optimizado2: %d",  fin3-ini3);
+    //printf("Invertido: %s\n", str);
+
+    ini4 = clock();
+    //QueryPerformanceCounter(&t_ini);
+
+    for (int i = 0; i < 1000001; i++)
+    {
+        invertir_optimizado_3(str);
+    }
+    
+    fin4 = clock();
+    //printf("Invertido: %s\n", str);
+    printf("\nMilisegundos optimizado3: %d",  fin4-ini4);
+
+    return 0;
+}
